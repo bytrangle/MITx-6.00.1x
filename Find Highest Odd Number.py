7cffc244a0fa62b24ba3227d1778069ad7bbb85d
trangthule <lethutrang101@gmail.com>
2017-06-20 16:11:55 +0700

    Update Find Highest Odd Number.py

--- Find Highest Odd Number.py
+++ Find Highest Odd Number.py
@@ -1,5 +1,8 @@
 #Finger exercise 2.2
 # Write a program that examines three variables -- x, y, and z -- and prints the largest odd number among them. If none of them are odd, it should print a message to that effect.
+x = int(input('Please enter x number:'))
+y = int(input('Please enter y number:'))
+z = int(input('Please enter z number:'))
 max = 0
 if x % 2:
   max = x
